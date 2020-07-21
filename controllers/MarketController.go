0a5f499221f3398b2290b2698a9e293926fa8cf5
tzj1042 <31153752+tzj1042@users.noreply.github.com>
2020-07-21 19:27:38 +0800

    添加功能

--- controllers/MarketController.go
+++ controllers/MarketController.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
+	"moneyBag/models"
 	"moneyBag/service"
 	"moneyBag/utils"
 )
@@ -12,11 +14,25 @@ type MarketController struct {
 
 func (c *MarketController) Get() {
 	defer c.ServeJSON()
-	name:= c.GetString("name")
+	name := c.GetString("name")
 	list, err := service.ListNewMarket(name)
 	if err != nil {
 		c.Data["json"] = utils.ErrReturn(err.Error())
 	} else {
 		c.Data["json"] = utils.SuccessReturn(list)
+	}
+}
+
+func (c *MarketController) Post() {
+	defer c.ServeJSON()
+	name := c.GetString("name")
+	unitPrice := c.GetString("unitPrice")
+	logs.Info("添加价格:", name)
+	newMarket := models.NewMarket{Name: name, UnitPrice: unitPrice}
+	data, err := service.AddNewMarket(newMarket)
+	if err != nil {
+		c.Data["json"] = utils.ErrReturn(err.Error())
+	} else {
+		c.Data["json"] = utils.SuccessReturn(data)
 	}
 }
--- service/MarketService.go
+++ service/MarketService.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"github.com/astaxie/beego/logs"
 	"moneyBag/models"
 	"moneyBag/mysql"
 )
@@ -10,4 +11,25 @@ func ListNewMarket(name string) ([]models.NewMarket, error) {
 	list := make([]models.NewMarket, 0)
 	err := mysql.Engine.Desc("id").Find(&list)
 	return list, err
+}
+
+//添加市场价格
+func AddNewMarket(newMarket models.NewMarket) (models.NewMarket, error) {
+	var oldNewMarket models.NewMarket
+	has, err := mysql.Engine.Where("name=?", newMarket.Name).Get(&oldNewMarket)
+	if err != nil {
+		logs.Error("异常:", err)
+		return newMarket, err
+	}
+	var history models.HistoryMarket
+	if has {
+		oldNewMarket.UnitPrice = newMarket.UnitPrice
+		_, err = mysql.Engine.ID(oldNewMarket.Id).Update(&oldNewMarket)
+		history = models.HistoryMarket{NewMarketId: oldNewMarket.Id, UnitPrice: newMarket.UnitPrice}
+	} else {
+		_, err = mysql.Engine.Insert(&newMarket)
+		history = models.HistoryMarket{NewMarketId: newMarket.Id, UnitPrice: newMarket.UnitPrice}
+	}
+	_, err = mysql.Engine.Insert(&history)
+	return newMarket, err
 }
